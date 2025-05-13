d425e9db96e6b75363763c1bc18cafe1db2bb76a
Jyothi2005 <JyothiHudali05@gmail.com>
2025-05-13 16:00:38 +0530

    add Fibonacci Program

---
+++ Course/Fibonacci.java
@@ -0,0 +1,15 @@
+package Course;
+    public class Fibonacci{
+        public static void main(String[] args) {
+            int n = 10; // Number of Fibonacci numbers to generate
+            int a = 0, b = 1;
+            System.out.println("Fibonacci Series:");
+            for (int i = 0; i < n; i++) {
+                System.out.print(a + " ");
+                int next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+    
+}
